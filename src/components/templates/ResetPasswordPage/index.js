0c0f5d277656c713bc3afa4148409f557eb6968a
JeremTer <ternisien09@gmail.com>
2022-06-30 14:20:21 +0200

    [ADD] : Reset password validation

--- src/components/templates/ResetPasswordPage/index.js
+++ src/components/templates/ResetPasswordPage/index.js
@@ -2,32 +2,37 @@ import React from 'react'
 import styled from 'styled-components'
 import { get } from 'lodash'
 import { useFormik } from 'formik'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import * as Yup from 'yup'
 
 import Form from '../../atoms/Form'
-import Error from '../../atoms/Error'
 import Input from '../../atoms/Input'
 import Title from '../../atoms/Title'
 import Button from '../../atoms/Button'
 import logoNovu from '../../../assets/svg/Logo.svg'
-import authErrorSelector from '../../../redux/selectors/authError'
-import { loginUser } from '../../../redux/slices/authentication'
-// import { useNavigate } from 'react-router-dom'
 
 const Logo = styled.img`
   width: auto;
   height: 200px;
 `
 
-const loginSchema = Yup.object().shape({
-  email: Yup.string().required(),
-  password: Yup.string().required()
+const validationSchema = Yup.object().shape({
+  password: Yup.string()
+    .required('Veuillez renseigner un nouveau mot de passe')
+    .min(8, 'Le mot de passe doit faire 8 caractères au minimum')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.{8,})/,
+      'Le mot de passe doit contenir au moins une majuscule'
+    ),
+  confirmPassword: Yup.string()
+    .required('Veuillez confirmer le mot de passe')
+    .oneOf([Yup.ref('password'), null],
+      'Les mots de passe doivent correspondre')
 })
 
 const StyledInput = styled(Input)`
   margin: 8px;
-  width: 200px;
+  width: 260px;
 `
 
 const StyledButton = styled(Button)`
@@ -71,18 +76,17 @@ const StyledForm = styled(Form)`
 
 const ResetPasswordPage = () => {
   const dispatch = useDispatch()
-  const authError = useSelector(authErrorSelector)
 
   const { values, errors, handleChange, handleSubmit } = useFormik({
     initialValues: {
       password: '',
       confirmPassword: ''
     },
-    validationSchema: loginSchema,
+    validationSchema: validationSchema,
     onSubmit: async (values) => {
-      await dispatch(loginUser(values))
-
-      return console.log('champs envoyé')
+      //await dispatch(loginUser(values))
+      console.log(values)
+      //return console.log('champs envoyé')
     }
   })
 
@@ -90,25 +94,35 @@ const ResetPasswordPage = () => {
     <ContainerRoot>
       <Container>
         <Logo src={logoNovu} />
-        <CenteredTitle color='primary'>Reinitialisé votre mot de passe</CenteredTitle>
+        <CenteredTitle color="primary">Réinitialisez votre mot de passe</CenteredTitle>
         <Container2>
           <StyledForm>
-            <Error error={authError}/>
             <StyledInput
-              type='password'
-              name='password'
-              error={!!get(errors, 'email')}
-              label="mot de passe"
-              value={get(values, 'password')}
-              onChange={handleChange} />
-            <StyledInput
-              type='password'
-              name='confirmPassword'
+              type="password"
+              name="password"
               error={!!get(errors, 'password')}
-              label='Mot de passe'
+              label="Mot de passe"
               value={get(values, 'password')}
               onChange={handleChange} />
-            <StyledButton background={'primary'} color={'white'} onClick={handleSubmit}>Valider</StyledButton>
+            <StyledInput
+              type="password"
+              name="confirmPassword"
+              error={!!get(errors, 'confirmPassword')}
+              label="Confirmez le mot de passe"
+              value={get(values, 'confirmPassword')}
+              onChange={handleChange} />
+            <Container2>
+              {Object.values(errors).map(msg => (
+                <>
+                  {msg}
+                  <br />
+                </>
+              ))}
+            </Container2>
+
+
+            <StyledButton type={'submit'} background={'primary'} color={'white'}
+                          onClick={handleSubmit}>Valider</StyledButton>
           </StyledForm>
         </Container2>
       </Container>
